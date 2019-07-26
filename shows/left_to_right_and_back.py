49cf1c9d5a448bf6b91b13cd1b7592409fcc31a4
John Major <john.e.major.jr@gmail.com>
2019-07-26 03:08:10 -0700

    working LeftToRightAndBack

--- shows/left_to_right_and_back.py
+++ shows/left_to_right_and_back.py
@@ -1,31 +1,56 @@
-from color import RGB
+from color import HSV, RGB
 from .showbase import ShowBase
 from grid import TriangleGrid, traversal
-
+import time
 
 class LeftToRightAndBack(ShowBase):
-    def __init__(self, tri_grid: TriangleGrid, frame_delay: float = 1.0):
+    def __init__(self, tri_grid: TriangleGrid, frame_delay: float = 0.2):
         self.tri_grid = tri_grid
         self.frame_delay = frame_delay
 
     def next_frame(self):
         row_count = self.tri_grid.row_count
-        fwd = True
 
+        hsv = HSV(0.0,0.9,.5, False)
+#        from IPython import embed; embed() 
+        pix_arr = []
+        a_ctr = 0
+        for points in traversal.left_to_right(row_count):
+ 
+            for (row, column) in points:
+                cell = self.tri_grid.get_cell_by_coordinates(row, column)
+                b_ctr = 0
+                for pixel in self.tri_grid._model._pixelmap[cell.id]:
+                    if len(pix_arr) <= a_ctr+b_ctr:
+                        pix_arr.append([])
+                    pix_arr[a_ctr+b_ctr].append(pixel)
+                    b_ctr += 1
+            a_ctr += 4
+        self.tri_grid.clear()
+        
         while True:
-            if fwd:
-                sequence = traversal.left_to_right(row_count)
-            else:
-                sequence = traversal.right_to_left(row_count)
+            for i in pix_arr:
+                
+                for ii in i:
+                    print(ii, hsv.hsv, hsv.rgbw)
+                    self.tri_grid._model.set_pixel_by_pixel_id(ii,hsv)
+                    self.tri_grid.go()
+                hsv.h += .09
+                if hsv.h >= 1.0:
+                    hsv.h = 0.0
+                time.sleep(0.9)
+ 
+            for i in reversed(pix_arr):
 
-            for points in sequence:
-                self.tri_grid.clear()
+                for ii in reversed(i):
+                    print(ii, hsv.hsv, hsv.rgbw)
+                    self.tri_grid._model.set_pixel_by_pixel_id(ii,hsv)
+                    self.tri_grid.go()
+                hsv.v += .1
+                if hsv.v >= 1.0:
+                    hsv.v = 0.0
+                time.sleep(0.9)
 
-                for (row, column) in points:
-                    print(f'row={row} column={column}')
-                    self.tri_grid.set_cell_by_coordinates(row, column, RGB(255, 255, 25))
+                
+            yield self.frame_delay
 
-                self.tri_grid.go()
-                yield self.frame_delay
-
-            fwd = not fwd  # Flips direction
